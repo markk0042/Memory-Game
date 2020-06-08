cf4704d3bbb77a3583994bff09c029a4db2c1574
Mark Kelly <markk0042@gmail.com>
2020-06-08 10:13:21 +0000

    updated js added flip functionality, also styled css to transform and change also and added if statement to js for true or false

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const cards = document.querySelectorAll(`.memory-card`);
 
-function flipcard(){
-    console.log(`i was clicked!`);
-    console.log(this);
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
+
+
+function flipcard(){
+    this.classList.toggle(`flip`);//toggle means if the class is there remove it if its not them add it.//
+
+    if (!hasFlippedCard){
+        //firstclick
+        hasFlippedCard = true;
+        firstCard = this;
+    
+    }else{
+        //secondclick
+        hasFlippedCard =false;
+        secondCard = this;
+
+        console.log({firstCard, secondCard});
+    
+    }
 }
 cards.forEach(card => card.addEventListener(`click`, flipcard));
