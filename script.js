0b153b9f38e726344c1136a716fb2bd4ba5a3dd9
Mark Kelly <markk0042@gmail.com>
2020-06-08 11:26:56 +0000

    updated js cleaned it up added more functions for resetting the board also updated data-framework in class of html document so images reflip when not matched and locked board function, changed timeout function, updated some css for perspective px

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
 const cards = document.querySelectorAll(`.memory-card`);
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
+function flipcard() {
+if (lockBoard)return;
+if (this === firstCard) return;
 
+  this.classList.add(`flip`); 
 
-function flipcard(){
-    this.classList.toggle(`flip`);//toggle means if the class is there remove it if its not them add it.//
+  if (!hasFlippedCard) {
+    //firstclick
+    hasFlippedCard = true;
+    firstCard = this;
 
-    if (!hasFlippedCard){
-        //firstclick
-        hasFlippedCard = true;
-        firstCard = this;
-    
-    }else{
-        //secondclick
-        hasFlippedCard =false;
-        secondCard = this;
+    return;
+  }
+    //secondclick
+    secondCard = this;
 
-        console.log({firstCard, secondCard});
-    
-    }
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === 
+    secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unFlipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener(`click`, flipCard);
+    secondCard.removeEventListener(`click`, flipCard);
+
+     resetBoard();
+}
+
+function unFlipCards(){
+lockBoard = true;
+
+    setTimeout(() => {
+      firstCard.classList.remove(`flip`);
+      secondCard.classList.remove(`flip`);
+
+        resetBoard();
+    }, 1100); 
+
+}
+
+function resetBoard(){
+
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 cards.forEach(card => card.addEventListener(`click`, flipcard));
